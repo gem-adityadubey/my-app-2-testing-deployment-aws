c28aae284e6e2273323e6d2c36e64f926347a6d0
gem-adityadubey <160013051+gem-adityadubey@users.noreply.github.com>
2024-12-02 12:25:44 +0530

    Update script.js
    updated the EC2 instance public ip.

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 async function fetchMessage() {
     try {
-      const response = await fetch("http://<EC2_PUBLIC_IP>:3000/api/health");
+      const response = await fetch("http://54.145.62.2:3000/api/health");
       const data = await response.json();
       document.getElementById("api-message").innerText = data.message;
     } catch (error) {
